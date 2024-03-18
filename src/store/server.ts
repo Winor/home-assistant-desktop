015e4046223929948cb70734ffd325c70836a7dd
Winor <or123123@gmail.com>
2024-03-18 17:25:26 +0200

    clean code

--- src/store/server.ts
+++ src/store/server.ts
@@ -30,32 +30,6 @@ interface ServerInstance {
     app: AppConfig | null,
 }
 
-// class Server {
-//     protocol: Protocol
-//     port: number
-//     address: string
-//     token: string
-//     // name: string
-//     // url: URL
-    
-//     constructor(login: ServerParams) {
-//         this.protocol = login.protocol
-//         this.address = login.address
-//         this.port = login.port
-//         this.token = login.token
-//         // this.name = `${this.address}:${this.port}`
-//         // this.url = new URL(`${this.protocol}${this.address}:${this.port}`)
-//     }
-
-//     get name() {
-//         return `${this.address}:${this.port}`
-//     }
-
-//     get url() {
-//         return new URL(`${this.protocol}${this.address}:${this.port}`)
-//     }
-// }
-
 type Config = Map<string, string>
 
 class InstanceList {
@@ -65,7 +39,7 @@ class InstanceList {
         this._list = writable<Config>(new Map)
     }
 
-    get list () {
+    get list() {
         return (async () => {
             await this.updateList();
             return this._list;
@@ -74,31 +48,35 @@ class InstanceList {
 
     async add(server: ServerParams, app?: AppConfig | null) {
         try {
-            const entry = await invoke("add_instance", {server: {...server}, app: null, })
+            const entry = await invoke("add_instance", { server: { ...server }, app: null, })
             await this.updateList()
-        } catch(err) {
+        } catch (err) {
             alert(err)
         }
     }
 
     async remove(id: string) {
-        await invoke("remove_instance", {id: id})
-        await this.updateList()
+        try {
+            await invoke("remove_instance", { id: id })
+            await this.updateList()
+        } catch (err) {
+            alert(err)
+        }
     }
 
-    find(url: URL) {
-        // return get(this._list).find(v => v.url.toString() === url.toString())
-    }
-
-    async updateList(){
-        const list = await invoke("get_list")
-        const map = new Map(Object.entries(list))
-        this._list.set(map);
+    async updateList() {
+        try {
+            const list = await invoke("get_list")
+            const map = new Map(Object.entries(list))
+            this._list.set(map);
+        } catch (err) {
+            alert(err)
+        }
     }
 
     subscribe(run: Subscriber<Config>) {
-		return this._list.subscribe(run)
-	}
+        return this._list.subscribe(run)
+    }
 
 }
 
