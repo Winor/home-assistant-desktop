f6a63213bccbe35485a13851e5013664fa196eb5
Winor <or123123@gmail.com>
2024-03-18 13:39:03 +0200

    add system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,18 +10,24 @@ use plugins::{
     config::{add_instance, get_list, remove_instance},
     ha_client::{hass_connect, hass_states, Haconn},
 };
-use tauri::Manager;
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayMenu};
 use tokio::sync::Mutex;
 use utils::config::{AppStore, Config};
 
 #[tokio::main]
 async fn main() {
     tauri::Builder::default()
+        .system_tray(
+            SystemTray::new().with_menu(
+                SystemTrayMenu::new()
+                    .add_item(CustomMenuItem::new("settings", "Settings"))
+                    .add_item(CustomMenuItem::new("exit", "Exit")),
+            ),
+        )
         .setup(|app| {
             let mut path = app.path_resolver().app_config_dir().unwrap();
             let _ = fs::create_dir(&path);
             path.push("config.yaml");
-            println!("{:?}", path);
             app.manage::<AppStore>(Mutex::new(Config::new(path)));
             Ok(())
         })
@@ -32,7 +38,33 @@ async fn main() {
             get_list,
             remove_instance
         ])
+        .on_system_tray_event(|app, event| match event {
+            tauri::SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "settings" => {
+                    let _window = tauri::WindowBuilder::new(
+                        app,
+                        "settings",
+                        tauri::WindowUrl::App("index.html".into()),
+                    )
+                    .title("Home assistant desktop Settings")
+                    .center()
+                    .build()
+                    .unwrap();
+                }
+                "exit" => {
+                    app.exit(0);
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .manage::<Haconn>(Mutex::new(None))
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
